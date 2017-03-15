c6729875fc384a0356afb752b5c8539fb956c5ed
giddygitau <n.gideon.gitau@gmail.com>
2017-03-15 18:47:29 +0300

    [Feature #140648421] Renames test to be more descriptive Adds url and url_detail to point to the modified endpoint Adds email to testing

--- wger/core/tests/test_register_user_api.py
+++ wger/core/tests/test_register_user_api.py
@@ -3,12 +3,14 @@ from wger.core.tests.api_base_test import ApiPostTestCase, ApiBaseTestCase
 from wger.core.tests.base_testcase import WorkoutManagerTestCase
 
 
-class UserRegistrationTests(WorkoutManagerTestCase, ApiBaseTestCase, ApiPostTestCase):
+class ApiUserRegistrationTests(WorkoutManagerTestCase, ApiBaseTestCase, ApiPostTestCase):
     """
     Test the API endpoint for registration registration
     of users
     """
+    url = '/api/v2/user/register/'
+    url_detail = '/api/v2/user/register/1/'
     resource = User
     private_resource = True
     special_endpoints = ()
-    data = {'username': 'giddy254', 'password': 'password'}
+    data = {'username': 'giddy254', 'email': 'giddy@example.com', 'password': 'password'}
