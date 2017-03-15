a8fe5eea15cbc7de1ed697e14351c0d6d4074e36
giddygitau <n.gideon.gitau@gmail.com>
2017-03-15 10:39:32 +0300

    [Feature #140648421] Makes urls more dynamic One endpoint to ease testing of creation and listing of users

--- wger/core/tests/test_register_user_api.py
+++ wger/core/tests/test_register_user_api.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.models import User
-from wger.core.tests.api_base_test import ApiPostTestCase
+from wger.core.tests.api_base_test import ApiPostTestCase, ApiBaseTestCase
+from wger.core.tests.base_testcase import WorkoutManagerTestCase
 
 
-
-class UserRegistrationTests(ApiPostTestCase):
+class UserRegistrationTests(WorkoutManagerTestCase, ApiBaseTestCase, ApiPostTestCase):
     """
     Test the API endpoint for registration registration
     of users
     """
     resource = User
-    url = '/api/v2/user/registration/'
+    private_resource = True
+    special_endpoints = ()
     data = {'username': 'giddy254', 'password': 'password'}
-
