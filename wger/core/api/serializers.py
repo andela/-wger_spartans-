966cdc821747605acc19a606e9910655ec5e4cf4
CharlesJonah <mutua.charles48@gmail.com>
2017-03-17 17:52:20 +0300

    [Feature #140648419] Adds a serializer used to serialize data from User model

--- wger/core/api/serializers.py
+++ wger/core/api/serializers.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Workout Manager.  If not, see <http://www.gnu.org/licenses/>.
 
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from wger.core.models import (
@@ -79,3 +80,27 @@ class WeightUnitSerializer(serializers.ModelSerializer):
     '''
     class Meta:
         model = WeightUnit
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    
+    password = serializers.CharField()
+    class Meta:
+        
+        model = User
+        fields = ('username', 'email', 'password')
+        
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],    
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class AllUserInfoSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+    
+    
